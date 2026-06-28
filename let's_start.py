75fffe3ee383914a5de1d969afce6ab5df177389
Suryaraj Sinh Jadeja <suryarajjadeja09@gmail.com>
2026-06-28 13:50:17 +0530

    here we go again

---
+++ let's_start.py
@@ -0,0 +1,12 @@
+line01 = "***************"
+line02 = "*             *"
+line03 = "*    Welcome! *"
+line04 = "*             *"
+line05 = "***************"
+
+print()
+print(line01)
+print(line02)
+print(line03)
+print(line04)
+print(line05)
