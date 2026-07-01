f5ff3b3913ca358b90b391e2e03648950d89a27e
Suryaraj Sinh Jadeja <suryarajjadeja09@gmail.com>
2026-07-01 13:19:05 +0530

    learned recusrion

--- funciton.py
+++ funciton.py
@@ -1,6 +1,42 @@
-def hello_world():
-    print("Hello World")
+# def hello_world():
+#     print("Hello World")
 
-hello_world()
+# hello_world()
 
 # let's talk about naming function the same way we do for the variable hahahahahaha
+
+# def sum(num1, num2):
+#     print(num1 + num2)
+
+# sum(42, 42)
+
+# def square_root(num):
+#     print (num*num)
+
+# square_root(4)
+
+def sum(num1 = 0 , num2 = 0):
+    if (type(num1) is not int or type(num2) is not int):
+        return 0
+    return num1 + num2
+
+total = sum()
+print(total)
+
+def multiple_items(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+multiple_items(4,5,6)
+print(total)
+
+def mult_named_items(**kwargs): #keyword_arguments
+    print(kwargs)
+    print(type(kwargs))
+    # Print the "Hello World" message. "Hello World" is stored in variable named data. 
+
+mult_named_items(first="Dave", last="Gray")
+
+
---
+++ recursion.py
@@ -0,0 +1,17 @@
+
+
+def add_one(num):
+    if (num>=9):
+        return num + 1 # Base code where recurssion will stop
+    
+    total = num + 1 # Recursive call for the function
+    print(total)
+
+    return add_one(total)
+
+
+# Call function for the initial value which is 0
+mynewtotal = add_one(0)
+print(mynewtotal)
+
+
