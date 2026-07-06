26ba6d759fa8698dfafe8cf94235bfae7c5274d6
Suryaraj Sinh Jadeja <suryarajjadeja09@gmail.com>
2026-07-06 23:36:39 +0530

    Exception_handling

---
+++ exception.py
@@ -0,0 +1,19 @@
+class justnotcoolerror(Exception):
+    pass
+
+
+x = 2
+try:
+    #print(x / 2)
+    raise justnotcoolerror("this is just isn't cool men")
+except NameError:
+    print("Name error means something is probably undefined.")
+except ZeroDivisionError:
+    print("please do not divide by 0")
+except TypeError as error:
+    print(error)
+else:
+    print("No errors!")
+finally:
+    print("This finally executes every time")
+
