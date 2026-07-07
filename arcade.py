dbde453916379d94075fbcd4906467828f0a50d1
Suryaraj Sinh Jadeja <suryarajjadeja09@gmail.com>
2026-07-07 11:24:27 +0530

    made some changes to the arcade game for better UI/UX

--- arcade.py
+++ arcade.py
@@ -3,9 +3,9 @@ from rps8 import rps
 from guess_num import guess_game
 
 def play_arcade(name):
-    print("\n Welcome to the arcade! \n")
-    print("\n Here are the games you can play:\n")
-    playerchoice = input(f"which game do you want to play\n1. rock_paper_scissors\n2. guess_num\n")
+    print(f"\n Welcome to the arcade! {name} \n")
+    print(f"\n Here are the games you can play {name}:\n")
+    playerchoice = input(f"which game do you want to play {name}, \n1. rock_paper_scissors\n2. guess_num\n")
     if playerchoice == "1":
         rps(name)
     elif playerchoice == "2":
@@ -16,9 +16,9 @@ def play_arcade(name):
         return play_arcade(name)
 
     while True:
-        playagain = input(f"\n play again?, \n Type Y for yes \n Type Q to quit \n\n")
+        playagain = input(f"\n {name} play again?, \n Type Y for yes \n Type Q to quit \n\n")
         if playagain.lower() not in ["y","q"]:
-            print("Please choose from the parameters given above")
+            print(f"\n Please {name} choose from the parameters given above\n")
             continue
         else:
             break
@@ -26,7 +26,7 @@ def play_arcade(name):
     if playagain.lower() == "y":
         return play_arcade(name)
     else:
-        print("\nThank you for playing!")
+        print(f"\nThank you {name}for playing!")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
