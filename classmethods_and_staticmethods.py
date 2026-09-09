3d0172216926876a1695ddbe9d495ad0d799b6b4
Suryaraj Sinh Jadeja <suryarajjadeja09@gmail.com>
2026-09-09 15:01:55 +0530

    decorators

--- classmethods_and_staticmethods.py
+++ classmethods_and_staticmethods.py
@@ -1,3 +1,4 @@
+import datetime
 class user:
     def __init__(self, name, age):
         self.name = name
@@ -8,7 +9,15 @@ class user:
         name, age = user_string.split('-')
         return cls(name, int(age))
 
+    @staticmethod
+    def workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            print("It's a weekend")
+        print("it's a workday")
+        
+
 user_string = "Suryaraj-23"
 user_1 = user.from_string(user_string)
+user_1.workday(datetime.date(2024, 12, 25))
 print(user_1.name)
 print(user_1.age)
